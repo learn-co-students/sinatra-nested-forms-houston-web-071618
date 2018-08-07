6553c9ad72b74811cd4936278af061cfa4e18153
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-07 16:39:37 -0500

    turning in lab

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  @@all = []
+  attr_reader :name, :weight, :height
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all.push(self)
+  end
+
+  def self.all
+    return @@all
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+@@all = []
+attr_accessor :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all.push(self)
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.clear
+    @@all = []
+    return @@all
+  end
 end
