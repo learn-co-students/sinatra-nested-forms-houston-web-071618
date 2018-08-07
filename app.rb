d7d340cba0a0e00722061999877150fe711f72bd
pabloargueta <pabloargueta27@gmail.com>
2018-08-07 16:41:12 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,33 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+
+      erb :'/root'
+    end
+
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      # binding.pry
+      @pirate = Pirate.new(params["pirate"])
+      # binding.pry
+      # Ship.create(params[:pirate][:ships])
+      params["pirate"]["ships"].each do |ship_info|
+        Ship.new(ship_info)
+      end
+
+      @ships = Ship.all
+      # binding.pry
+
+      erb :'pirates/show'
+    end
+    
+
+
+
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate
+
+  @@all = []
+
+  attr_reader :name, :weight, :height
+
+  def initialize(args)
+    @name = args["name"]
+    @weight = args["weight"]
+    @height = args["height"]
+
+    @@all << self
+  end
+
+  # def self.all
+  #   @@all
+  # end
+
+  # def self.clear
+  #   @@all = []
+  # end
+
+
+
 end
