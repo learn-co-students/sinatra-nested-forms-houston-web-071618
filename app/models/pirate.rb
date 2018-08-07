d2fd6412bc0dadbb77d621297b13fc8b6599a9be
github username <github email address>
2018-08-07 13:48:05 -0500

    classes set up

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+
+  @@all = []
+
+  def initialize
+    @name
+    @weight
+    @height
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  @@all = []
+
+  def initialize
+    @name
+    @type
+    @booty
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.clear
+    self.all = []
+  end
+
 end
