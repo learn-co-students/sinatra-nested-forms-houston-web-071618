b18f8ff016b47b21d08da7c3defd1226dedde54c
Sean Huang <hyprul@ucla.edu>
2018-08-07 14:40:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,33 @@ module FormsLab
 
     # code other routes/actions here
 
+    # configure do
+    # 	set :views, 'views/pirates'
+    # end
+
+
+    get '/' do
+
+    	erb :root
+    end
+
+    get '/new' do
+
+    	erb :'../views/pirates/new'
+    end
+
+
+    post '/pirates' do
+    	
+    	@pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height] )
+    	#binding.pry
+    	@ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+    	@ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+    	#binding.pry
+
+    	erb :'../views/pirates/show'
+    end
+
+
   end
 end
